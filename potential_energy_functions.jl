73ca4f6b23912303a6cf371da2bd8e49b61c5ffd
mymajorig <phoebe.t@wustl.edu>
2025-07-07 09:31:10 -0500

    organization changes

--- potential_energy_functions.jl
+++ potential_energy_functions.jl
@@ -5,8 +5,9 @@ function lennard_jones(distance, epsilon = 1, sigma = 1) #defaults, if u change 
       return 4 * epsilon * ((sigma / distance)^ 12 - (sigma / distance)^ 6)
 end
 
+#--
 #sum of potential energy between multiple pairs by adding up the lj vals of each distance 
-function d_sum_lj_energy(distance) #takes in a list of distances 
+function sum_lj_energy(distance) #takes in a list of distances 
     value  = 0
     for i in distance 
         value+=lennard_jones(i)
@@ -16,32 +17,83 @@ function d_sum_lj_energy(distance) #takes in a list of distances
 end 
 
 #sum of potential energy given one distance val and one theta val 
-function rtheta_sum_lj_energy(distance, theta)
+function sum_lj_energy(distance, theta)
     if abs(cos(theta)) < 1e-6
     error("theta too close to ±90 degrees, causing division by zero.")
     end
 
     atom1 = [0,0]
-    atom2 = [r12, 0]
+    atom2 = [distance, 0]
 
-    midpoint_distance = r12/2
+    midpoint_distance = distance/2
     midpoint = [midpoint_distance, 0]
 
-   r13 = r12/(2*cos(theta))
+
+   r13 = distance/(2*cos(theta)) #half of the distance aka half of r12 divided by cos theta
 
    r23 = r13 
 
-   return lennard_jones(r12) + lennard_jones(r13) + lennard_jones(r23)
-
+   return lennard_jones(distance) + lennard_jones(r13) + lennard_jones(r23) 
 end 
+
+function sum_lj_energy_isosceles(distance, height)
+    atom1 = [0, 0]
+    atom2 = [distance, 0]
+    midpoint = [distance/2, 0]
+
+    atom3 = [distance/2, height]  # directly above midpoint, so isosceles
+
+    r12 = distance
+    r13 = sqrt((atom3[1] - atom1[1])^2 + (atom3[2] - atom1[2])^2)
+    r23 = r13  # equal sides 
+
+    return lennard_jones(r12) + lennard_jones(r13) + lennard_jones(r23)
+end
+
+function sum_lj_energy(distance, x3, y3) #by using coordinates or fixing one distance and positions, you automatically respect geometric constraints and ensure that the 3 distances in the situation end up forming a triangle
+    #assumption that atom 1 is at (0,0)
+    atom1 = [0,0]
+    atom2 = [distance, 0.0]
+    atom3 = [x3, y3]
+
+    r13 = norm(atom3 .- atom1) #the dot just emphasizes that it subtracts each element in the list
+    r23 = norm(atom3 .- atom2)
+
+    return lennard_jones(distance) + lennard_jones(r13) + lennard_jones(r23)
+end 
+
+
+
+
+#--- END OF TOTAL POT FUNCTIONS
 
 #calculate the distance between 2 atoms
 function distance_two_atoms(atom1, atom2)
     return norm(atom2 - atom1)
 end
 
-println(sum_lj_energy(2, 45))
+#for rounding numbers 
+function round_num(num::Number, digits)
 
+    places_moved = 10^digits
+    num *= places_moved
+    num = round(num) #round it
+    num/= 10^digits #move it however many decimal places back so u can get the original version 
 
+    return num 
+end
+
+function round_num(vector::AbstractVector, digits)
+    for i in eachindex(vector) 
+        vector[i] = round_num(vector[i], digits)
+    end
+    return vector
+end 
+
+vec = [2.8932, 5.9284]
+
+round_num(vec, 3)
+
+        
 
 
--- test.jl
+++ test.jl
@@ -1,8 +1,6 @@
  
 include("potential_energy_functions.jl")
 
-
-
 #TESTING LENNARD JONES
 lennard_jones(1.1)
 lennard_jones(2.4)
@@ -10,7 +8,10 @@ lennard_jones(2.4)
 #TESTING sum_lj_energy that takes in a list of distances
 
 distance_list = [10, 3, 4, 2]
-d_sum_lj_energy(distance_list)
+sum_lj_energy(distance_list)
+
+#testing sum_lj_energy with a distance and an angle
+sum_lj_energy(2, 45)
 
 #TESTING distance between 2 atoms
 a1 = [2, 5]
@@ -20,3 +21,10 @@ a4 = [15, 90]
 
 distance_two_atoms(a1, a2)
 distance_two_atoms(a3, a4)
+
+#testing rounding decimals 
+f = 6.2385279
+round_num(f, 5)
+round_num(f,4)
+
+
